bb80cab2ead2075bbf1c8211c3489df8c95651e9
ckester99 <christiankester77@gmail.com>
2023-07-23 19:24:27 -0400

    added transition animations

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,13 @@
-import Link from "next/link";
 import { LiaHomeSolid } from "react-icons/lia";
+import { FaRegChessKnight } from "react-icons/fa6";
+import { CgProfile } from "react-icons/cg";
 
 export default function Navbar() {
     return (
-        <div className="h-12">
-            <nav className=" bg-zinc-800 text-green-600 fixed t-0 p-4 w-screen flex flex-row-reverse gap-2">
-                <NavBarIcon navRoute={`/old`} icon={<LiaHomeSolid size={"32"} />}></NavBarIcon>
-                <NavBarIcon navRoute={`/old`} icon={<LiaHomeSolid size={"32"} />}></NavBarIcon>
+        <div className="h-16">
+            <nav className=" bg-zinc-800 text-green-600 fixed t-0 p-4 w-screen flex flex-row justify-end gap-3.5">
+                <NavBarIcon navRoute={`/old`} icon={<FaRegChessKnight size={"32"} />}></NavBarIcon>
+                <NavBarIcon navRoute={`/old`} icon={<CgProfile size={"32"} />}></NavBarIcon>
                 <NavBarIcon navRoute={`/old`} icon={<LiaHomeSolid size={"32"} />}></NavBarIcon>
             </nav>
         </div>
@@ -15,7 +16,9 @@ export default function Navbar() {
 
 function NavBarIcon({ navRoute, icon }) {
     return (
-        <a href={navRoute} className="bg-slate-800 h-12 w-12 flex flex-col justify-center items-center">
+        <a
+            href={navRoute}
+            className="bg-slate-200 h-16 w-16 flex flex-col justify-center items-center rounded-[2rem] hover:rounded-2xl transition-all duration-300 ease-linear">
             {icon}
         </a>
     );
