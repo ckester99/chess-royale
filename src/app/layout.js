1911b3fa6fe99927637c360c38b4f4ac3a74bc21
ckester99 <christiankester77@gmail.com>
2023-07-23 19:01:52 -0400

    added icon layout to navBar

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,9 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body className={`${inter.className} bg-slate-800 text-slate-100 container mx-auto p-4`}>
+            <body className={`${inter.className} bg-slate-800 text-slate-100`}>
                 <Navbar></Navbar>
-                {children}
+                <div className="container mx-auto p-4 pt-12">{children}</div>
             </body>
         </html>
     );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,16 +3,20 @@ import { LiaHomeSolid } from "react-icons/lia";
 
 export default function Navbar() {
     return (
-        <nav className="">
-            <Icon navRoute={"/old"} img={LiaHomeSolid}></Icon>
-        </nav>
+        <div className="h-12">
+            <nav className=" bg-zinc-800 text-green-600 fixed t-0 p-4 w-screen flex flex-row-reverse gap-2">
+                <NavBarIcon navRoute={`/old`} icon={<LiaHomeSolid size={"32"} />}></NavBarIcon>
+                <NavBarIcon navRoute={`/old`} icon={<LiaHomeSolid size={"32"} />}></NavBarIcon>
+                <NavBarIcon navRoute={`/old`} icon={<LiaHomeSolid size={"32"} />}></NavBarIcon>
+            </nav>
+        </div>
     );
 }
 
-function Icon({ navRoute, img }) {
+function NavBarIcon({ navRoute, icon }) {
     return (
-        <Link href={`${navRoute}`}>
-            <LiaHomeSolid></LiaHomeSolid>
-        </Link>
+        <a href={navRoute} className="bg-slate-800 h-12 w-12 flex flex-col justify-center items-center">
+            {icon}
+        </a>
     );
 }
